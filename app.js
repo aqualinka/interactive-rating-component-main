fe6e2f25b1a002d3fac53488f36896b7607c3ff5
aqualinka <michaela.kovaja@gmail.com>
2023-01-03 20:40:15 +0000

    update js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function handleSubmit(event){
     event.preventDefault();
-    let input = document.querySelector('input[name="ratingBtn"]:checked');
+    let input = document.querySelector('input[name="ratingBtn"]:checked').value;
     
+   
         document.querySelector(".card-rating").style.display = `none`;
         document.querySelector(".card-thankyou").style.display = `flex`;
-        document.getElementById("rating-message").innerHTML = `You selected ${input.value} out of 5`;
-     }
-   
+        document.getElementById("rating-message").innerHTML = `You selected ${input} out of 5`;
+    
 let form=document.getElementById("form");
 form.addEventListener("submit",handleSubmit);
 
 
-  
+}
