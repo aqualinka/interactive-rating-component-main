659613ac426acf2646ecbfc548f884616d8b77d2
aqualinka <michaela.kovaja@gmail.com>
2023-05-10 13:24:23 +0100

    added message if input empty

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 function handleSubmit(event){
     event.preventDefault();
-    let input = document.querySelector('input[name="ratingBtn"]:checked').value;
-    
-   
+    let input = document.querySelector('input[name="ratingBtn"]:checked');
+    if (!input) {
+        alert("You need to rate us first!");
+        return;
+      }
         document.querySelector(".card-rating").style.display = `none`;
         document.querySelector(".card-thankyou").style.display = `flex`;
-        document.getElementById("rating-message").innerHTML = `You selected ${input} out of 5`;
+        document.getElementById("rating-message").innerHTML = `You selected ${input.value} out of 5`;
     
 }
 let form=document.getElementById("form");
